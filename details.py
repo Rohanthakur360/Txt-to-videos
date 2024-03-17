b7148bf38b708e2696b36ecfc9660df0d63cb85f
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-03-17 13:53:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = "yourApi"  
-api_hash = "yuour Apihash"
-bot_token = "Your Bot Token"
-sudo_groups = "-100yourgroup"
+api_id = "27464924"  
+api_hash = "f28d089001481597f045b70a0daa73e4"
+bot_token = "6505721336:AAFai0KgWNTnXcBHG_UcmQbwnjGbn05BHtw"
+sudo_groups = "-1002027934288"
 
