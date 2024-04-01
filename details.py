9c0d5e39ed6ca56b60c6411db5f864ba1b523c4b
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-01 08:18:38 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = "20622046"  
-api_hash = "334117de36364fa82e42c67dd45a1974"
-bot_token = "6089978385:AAGpbKImZh5mpemT5ZapjMfjSWu97i4pvh8"
-sudo_groups = "-1002132785565"
+api_id = "20626"  
+api_hash = "334117de34"
+bot_token = "112453533:fjakrja532af3a1r21afh8"
+sudo_groups = "-10025513658453"
 
